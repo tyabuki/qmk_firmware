bf41550b82afb364572b286df205276a2f60113d
Toru Yabuki <yabuuuuu@gmail.com>
2019-03-29 00:44:42 +0900

    add mac mode :apple:

--- keyboards/mint60/keymaps/tyabuki/keymap.c
+++ keyboards/mint60/keymaps/tyabuki/keymap.c
@@ -32,53 +32,96 @@ enum custom_keycodes {
 
 // Layer Declarations
 enum {
-    _QUWERTY = 0,
-    _FN1 = 1,
-    _FN2 = 2,
+    _Q_MAC = 0,
+    _Q_WIN = 1,
+    _FN_MAC = 2,
+    _FN_WIN = 3,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
- * QWERTY
+ * QWERTY_MAC
  * ,--------------------------------------------------------------------------------------------------.
  * |  `   |   1  |   2  |   3  |   4  |   5  ||   6  |   7  |   8  |   9  |   0  |   -  |   =  | Bksp |
  * |------+------+------+------+------+------++------+------+------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  ||   Y  |   U  |   I  |   O  |   P  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------++------+------+------+------+------+------+------+------|
- * | Caps |   A  |   S  |   D  |   F  |   G  ||   H  |   J  |   K  |   L  |   ;  |   '  |    Enter    |
+ * |LCtrl |   A  |   S  |   D  |   F  |   G  ||   H  |   J  |   K  |   L  |   ;  |   '  |    Enter    |
  * |------+------+------+------+------+------++------+------+------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  ||   N  |   M  |   ,  |   .  |   /  | Fn2  |  Up  |RShift|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  ||   N  |   M  |   ,  |   .  |   /  |RShift|  Up  |RShift|
  * |-----------------------------------------++-------------------------------------------------------|
- * | Fn1 | LCtrl |  Win  |  LAlt |   Space   ||    Space       | RAlt   | Fn1    | Left | Down |Right |
+ * | Fn1 | LCtrl | LAlt  |  LCmd |   Space   ||    Space       | RCmd   | Fn1    | Left | Down |Right |
  * `--------------------------------------------------------------------------------------------------'
-*/
-  [_QUWERTY] = LAYOUT( \
-    KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,   KC_5,        KC_6,    KC_7,     KC_8,    KC_9,     KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  \
-    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,        KC_Y,    KC_U,     KC_I,    KC_O,     KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,  \
-    KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,        KC_H,    KC_J,     KC_K,    KC_L,     KC_SCLN, KC_QUOT,  KC_ENT,  \
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,        KC_N,    KC_M,     KC_COMM, KC_DOT,   KC_SLSH, MO(_FN2), KC_UP,   KC_RSFT,   \
-    MO(_FN1), KC_LCTL, KC_LWIN, KC_LALT, KC_SPC,              KC_SPC,            KC_RALT, MO(_FN1),          KC_LEFT,  KC_DOWN, KC_RGHT \
+ */
+  [_Q_MAC] = LAYOUT( \
+    KC_GRV,      KC_1,    KC_2,    KC_3,    KC_4,   KC_5,        KC_6,   KC_7, KC_8,    KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+    KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,        KC_Y,   KC_U, KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
+    KC_LCTL,     KC_A,    KC_S,    KC_D,    KC_F,   KC_G,        KC_H,   KC_J, KC_K,    KC_L,     KC_SCLN, KC_QUOT, KC_ENT,  \
+    KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,        KC_N,   KC_M, KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, KC_UP,   KC_RSFT, \
+    MO(_FN_MAC), KC_LCTL, KC_LALT, KC_LCMD, KC_SPC,              KC_SPC,       KC_RCMD, MO(_FN_MAC),       KC_LEFT, KC_DOWN, KC_RGHT \
   ),
 /*
- * Fn1
+ * Fn_MAC
  * ,--------------------------------------------------------------------------------------------------.
  * | Esc  |  F1  |  F2  |  F3  |  F4  |  F5  ||  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  | Del  |
  * |------+------+------+------+------+------++------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      ||      |      |      |      |      |      |      |      |
+ * |      |BriDwn|BriUp |      |      |BLInc ||BLDec | Prev | Play | Next | Mute |VolDwn|VolUp |      |
  * |------+------+------+------+------+------++------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      ||      |      |      |      |      |      |             |
+ * | Caps |      |      |      |      |      ||      |      |      |      |      |      |             |
  * |------+------+------+------+------+------++------+------+------+------+------+------+------+------|
- * | ---- |      |      |      |      |      ||      |      |      |      |      |      | PgUp | ---- |
+ * | ---- |      |      |      |      | WIN  || MAC  |      |      |      |      |      | PgUp | ---- |
  * |-----------------------------------------++-------------------------------------------------------|
  * |     | ----- | ----- | ----- | --------- || -------------- | ------ |        | Home | PgDn | End  |
  * `--------------------------------------------------------------------------------------------------'
-*/
-  [_FN1] = LAYOUT( \
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-    KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, KC_TRNS, \
-    XXXXXXX, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                 KC_TRNS,          KC_TRNS, XXXXXXX,          KC_HOME, KC_PGDN, KC_END \
+ */
+  [_FN_MAC] = LAYOUT( \
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,      KC_F12,    KC_DEL, \
+    XXXXXXX, KC_BRMD, KC_BRMU, XXXXXXX, XXXXXXX, BL_INC,          BL_DEC,     KC_MRWD, KC_MPLY, KC_MFFD, KC__MUTE, KC__VOLDOWN, KC__VOLUP, XXXXXXX, \
+    KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,   \
+    KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG(_Q_WIN),      TG(_Q_MAC), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,     KC_PGUP,   KC_TRNS, \
+    XXXXXXX, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                  KC_TRNS,             KC_TRNS, XXXXXXX,           KC_HOME,     KC_PGDN,   KC_END \
+  ),
+/*
+ * QWERTY_WIN
+ * ,--------------------------------------------------------------------------------------------------.
+ * |  `   |   1  |   2  |   3  |   4  |   5  ||   6  |   7  |   8  |   9  |   0  |   -  |   =  | Bksp |
+ * |------+------+------+------+------+------++------+------+------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  ||   Y  |   U  |   I  |   O  |   P  |   [  |   ]  |  \   |
+ * |------+------+------+------+------+------++------+------+------+------+------+------+------+------|
+ * |LCtrl |   A  |   S  |   D  |   F  |   G  ||   H  |   J  |   K  |   L  |   ;  |   '  |    Enter    |
+ * |------+------+------+------+------+------++------+------+------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  ||   N  |   M  |   ,  |   .  |   /  |RShift|  Up  |RShift|
+ * |-----------------------------------------++-------------------------------------------------------|
+ * | Fn1 | LCtrl |  Win  |  LAlt |   Space   ||    Space       | RAlt   | Fn1    | Left | Down |Right |
+ * `--------------------------------------------------------------------------------------------------'
+ */
+  [_Q_WIN] = LAYOUT( \
+    KC_GRV,       KC_1,    KC_2,    KC_3,    KC_4,   KC_5,        KC_6,   KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+    KC_TAB,       KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,        KC_Y,   KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
+    KC_LCTL,      KC_A,    KC_S,    KC_D,    KC_F,   KC_G,        KC_H,   KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,  \
+    KC_LSFT,      KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,        KC_N,   KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,   KC_RSFT, \
+    MO(_FN_WIN),  KC_LCTL, KC_LWIN, KC_LALT, KC_SPC,              KC_SPC,       KC_RALT, MO(_FN_WIN),     KC_LEFT, KC_DOWN, KC_RGHT \
+  ),
+/*
+ * Fn_WIN
+ * ,--------------------------------------------------------------------------------------------------.
+ * | Esc  |  F1  |  F2  |  F3  |  F4  |  F5  ||  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  | Del  |
+ * |------+------+------+------+------+------++------+------+------+------+------+------+------+------|
+ * |      | Mute |VolDwn|VolUp |      |BriDwn||BriUp |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------++------+------+------+------+------+------+------+------|
+ * | Caps |      | Pscr |      |      |      ||      |      |      |      |      |      |             |
+ * |------+------+------+------+------+------++------+------+------+------+------+------+------+------|
+ * | ---- |      |      |      |      | WIN  || MAC  |      |      |      |      |      | PgUp | ---- |
+ * |-----------------------------------------++-------------------------------------------------------|
+ * |     | ----- | ----- | ----- | --------- || -------------- | ------ |        | Home | PgDn | End  |
+ * `--------------------------------------------------------------------------------------------------'
+ */
+  [_FN_WIN] = LAYOUT( \
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
+    XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, KC_BRIU,         KC_BRID,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+    KC_CAPS, XXXXXXX, KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+    KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG(_Q_WIN),      TG(_Q_MAC), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, KC_TRNS, \
+    XXXXXXX, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                  KC_TRNS,             KC_TRNS, XXXXXXX,          KC_HOME, KC_PGDN, KC_END \
   ),
 /*
  * Fn2
@@ -93,14 +136,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------++-------------------------------------------------------|
  * |     | ----- | ----- | ----- | --------- || -------------- | ------ |        |      |      |      |
  * `--------------------------------------------------------------------------------------------------'
-*/
-[_FN2] = LAYOUT( \
+ */
+/*
+  [_FN2] = LAYOUT( \
     XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, KC_BRIU,        KC_BRID, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,   XXXXXXX, \
     XXXXXXX, KC_BRMD, KC_BRMU, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC__MUTE, KC__VOLDOWN, KC__VOLUP, XXXXXXX, \
     RGB_TOG, RGBRST,  RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,   \
     KC_TRNS, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,   KC_TRNS, \
     XXXXXXX, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                 KC_TRNS,          KC_TRNS, XXXXXXX,           XXXXXXX,     XXXXXXX,   XXXXXXX \
   )
+ */
 };
 
 // define variables for reactive RGB
